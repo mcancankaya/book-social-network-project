3de077e620a9bad312a72b57e787a91795a4057a
mcancankaya <mccankaya45@hotmail.com>
2024-07-17 21:17:19 +0300

    Implement the menu component

--- book-network-ui/src/app/modules/book/book.module.ts
+++ book-network-ui/src/app/modules/book/book.module.ts
@@ -2,10 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {BookRoutingModule} from './book-routing.module';
+import {MainComponent} from './pages/main/main.component';
+import {MenuComponent} from './components/menu/menu.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent,
+    MenuComponent
+  ],
   imports: [
     CommonModule,
     BookRoutingModule
