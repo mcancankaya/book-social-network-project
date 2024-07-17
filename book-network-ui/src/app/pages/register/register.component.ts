d17adc2754782ac7d36b965e1b01d55396d83b15
mcancankaya <mccankaya45@hotmail.com>
2024-07-17 12:40:52 +0300

    Implement the Registration page

---
+++ book-network-ui/src/app/pages/register/register.component.ts
@@ -0,0 +1,40 @@
+import {Component} from '@angular/core';
+import {RegistrationRequest} from "../../services/models/registration-request";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  registerRequest: RegistrationRequest = {email: '', firstName: '', lastName: '', password: ''};
+  errorMessage: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+  }
+
+  register() {
+    this.errorMessage = [];
+    this.authService.register({
+      body: this.registerRequest
+    }).subscribe(
+      {
+        next: () => {
+          this.router.navigate(['activate-acoount'])
+        },
+        error: (err) => {
+          this.errorMessage = err.error.validationErrors;
+        }
+      }
+    )
+  }
+
+  login() {
+    this.router.navigate(['login']);
+  }
+}
